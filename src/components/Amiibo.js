b7188bad42338d335e93ee30f701fc893d380f71
tannyque <tannyque1992@gmail.com>
2018-11-05 21:49:16 +0000

    Adds amiibo

---
+++ src/components/Amiibo.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Amiibo = (props) => {
+  return (
+    <div className="amiibo">
+      <h2>{props.amiibo.name}</h2>
+      <ul className="amiibo-info">
+        <li>Amiibo Series: {props.amiibo.amiiboSeries}</li>
+        <li>Character: {props.amiibo.character}</li>
+        <li>Game Series: {props.amiibo.gameSeries}}</li>
+        <li>Type: {props.amiibo.type}</li>
+      </ul>
+      <img src={props.amiibo.image} alt={props.amiibo.name} className="image"/>
+    </div>
+  );
+}
+
+export default Amiibo;
